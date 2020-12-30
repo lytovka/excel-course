f20f9f47194b0388614874f6a63635110b735895
Ivan Lytovka <ilytovka@homex.com>
2020-12-30 18:27:32 -0500

    feat: add event listener initialization and removal

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,8 +1,35 @@
+import {capitalize} from './utils'
+
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('No root was provided for DOM Listener')
     }
     this.$root = $root
+    this.listeners = listeners
+    this.activeListeners = []
   }
+
+  initDomListeners() {
+    this.listeners.forEach((listener) => {
+      const callbackName = getMethodName(listener)
+      if (!this[callbackName]) {
+        throw new Error(`Method ${callbackName} is not implemented in ${this.name} Component`)
+      }
+      const boundListener = this[callbackName].bind(this)
+      this.activeListeners.push({type: callbackName, listener: boundListener})
+      this.$root.on(listener, boundListener)
+    })
+  }
+
+  removeDomListeners() {
+    this.activeListeners.forEach((activeListener) => {
+      const {type, listener} = activeListener
+      this.$root.off(type, listener)
+    })
+  }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
